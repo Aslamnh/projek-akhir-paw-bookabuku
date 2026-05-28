d77e8169d1e33c6068280066d19a7ea048c95f52
narayama <zienard06@gmail.com>
2026-05-28 09:47:28 +0700

    fixed cart
    ga sengaja keoverride

--- resources/views/katalog.blade.php
+++ resources/views/katalog.blade.php
@@ -577,9 +577,19 @@
                             
                             <div class="price-container">
                                 <span class="book-price">Rp {{ number_format($book->price, 0, ',', '.') }}</span>
-                                <object><button class="cart-btn" title="Tambah ke Keranjang" onclick="event.preventDefault(); /* implement cart add */">
-                                    <img src="{{ asset('icon-images/cart.png') }}" alt="Cart" class="cart-icon">
-                                </button></object>
+                                    <form action="{{ route('cart.add', $book->id) }}" method="POST">
+                                        @csrf
+
+                                        <button type="submit"
+                                                class="cart-btn"
+                                                title="Tambah ke Keranjang">
+
+                                        <img src="{{ asset('icon-images/cart.png') }}"
+                                        alt="Cart"
+                                        class="cart-icon">
+
+                                        </button>
+                                    </form>
                             </div>
                         </div>
                     </a>
